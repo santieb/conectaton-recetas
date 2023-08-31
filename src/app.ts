7f0dfe6f5f2affedeedb6749a25cbc582202cf8b
Santiago Barreto <78882685+santieb@users.noreply.github.com>
2023-08-31 13:26:03 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import ExpressConfig from './config/express.config'
+import { PORT } from './config/config'
+
+const app = ExpressConfig()
+
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+import config from 'dotenv'
+
+config.config()
+
+export const PORT = process.env.PORT || 5000
