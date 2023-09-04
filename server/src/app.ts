0821db179f9b44cf9d3cb36f20ed0f0b9393edd8
Gustavo Sobol <gustavosobol@hotmail.com>
2023-09-03 22:25:15 -0300

    agrego dos endpoints al swagger

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,13 +2,14 @@ import ExpressConfig from './config/express.config';
 import { PORT } from './config/config';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+/* const path = require('path');
+
 // configuracion de rutas
 
 const moduleAlias = require('module-alias');
-const path = require('path');
 moduleAlias.addAliases({
   '@': path.resolve(__dirname),
-});
+}); */
 
 const app = ExpressConfig();
 
@@ -21,7 +22,7 @@ const options = {
       version: '1.0.0',
     },
     schemes: ['http', 'https'],
-    servers: [{ url: 'http://111.100.100.10:3000/' }],
+    servers: [{ url: `http://localhost:${PORT}/` }],
     components: {
       securitySchemes: {
         jwt: {
@@ -36,19 +37,18 @@ const options = {
   },
   // Rutas de los archivos que van a contener la documentaciones de para swaggger
   apis: [
-    /*     `${__dirname}/api/auth/routes/auth.routes.ts`,
-    './dist/api/auth/routes/auth.routes.ts',
+    `${__dirname}/endpoints/test_connection.ts`,
+    /*'./dist/api/auth/routes/auth.routes.ts',
     `${__dirname}/api/user/routes/users.routes.ts`,
     './dist/api/user/routes/users.routes.ts',
     `${__dirname}/api/person/routes/person.routes.ts`,
     './dist/api/person/routes/person.routes.ts', */
   ],
 };
+const testConnection = require('./endpoints/test_connection');
+app.use('/test', testConnection);
 
 const swaggerSpec = swaggerJSDoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-const test = require('./endpoints/test2');
-app.use('/test', test);
 
 app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
---
+++ server/src/endpoints/test_connection.ts
@@ -0,0 +1,122 @@
+import express, { NextFunction, Request, Response } from 'express';
+import axios from '../config/axios';
+
+const router = express.Router();
+const options = () => {
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  return options;
+};
+/**
+ *
+ * @swagger
+ * /test/connection:
+ *      get:
+ *          summary: Realiza un test para ver si la conexion al servidor esta funcionando
+ *          tags:
+ *              - Conexion
+ *          description: Realiza un test para ver si la conexion al servidor esta funcionando
+ *          responses:
+ *              200:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  text:
+ *                                      type: string
+ *                                      example: Retorna array con datos de la conexión!
+ *              400:
+ *                  description: Not found
+ *              404:
+ *                  description: Not found
+ *              500:
+ *                  description: Internal server error
+ *
+ */
+
+router.get(
+  '/connection',
+  async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const response = await axios.get('/metadata', options());
+      console.log(`print response - data ${JSON.stringify(response.data)}`);
+
+      res.json(response.data);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+/**
+ * @swagger
+ * /test/MedicationRequest:
+ *   get:
+ *     summary: Obtener una solicitud de medicación por identificador
+ *     description: Obtener una solicitud de medicación por identificador con opciones de inclusión.
+ *     parameters:
+ *       - in: query
+ *         name: identifier
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: El identificador de la solicitud de medicación.
+ *       - in: query
+ *         name: _include
+ *         required: false
+ *         schema:
+ *           type: string
+ *         description: Opciones de inclusión para la solicitud de medicación.
+ *     responses:
+ *       200:
+ *         description: Respuesta exitosa.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de éxito.
+ *                 data:
+ *                   type: object
+ *                   description: Datos de la solicitud de medicación.
+ *       400:
+ *         description: Error en la solicitud.
+ */
+
+router.get(
+  '/MedicationRequest',
+  async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Obtener los valores de los parámetros de consulta
+      const identifier = _req.query.identifier as string | undefined;
+      const include = _req.query._include as string | undefined;
+
+      // Crear un objeto de opciones para la solicitud Axios
+      const options = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        params: {
+          identifier,
+          _include: include,
+        },
+      };
+
+      const response = await axios.get('/MedicationRequest', options);
+      console.log(`print response - data ${JSON.stringify(response.data)}`);
+
+      res.json(response.data);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+module.exports = router;
